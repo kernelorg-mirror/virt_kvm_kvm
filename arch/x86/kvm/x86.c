740d48818c86326be8df2560cf7863435f276335
Paolo Bonzini <pbonzini@redhat.com>
2025-03-23 21:40:17 +0100

    KVM: pass plane to kvm_arch_vcpu_create
    Pass the plane to architecture-specific code, so that it can also share
    backing data between plane 0 and the non-zero planes.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12293,7 +12293,7 @@ int kvm_arch_vcpu_precreate(struct kvm *kvm, unsigned int id)
 	return kvm_x86_call(vcpu_precreate)(kvm);
 }
 
-int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
+int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu, struct kvm_plane *plane)
 {
 	struct page *page;
 	int r;
