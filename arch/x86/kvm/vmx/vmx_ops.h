ff6971751e238a4002bb5e77127654a98c10f7bb
Paolo Bonzini <pbonzini@redhat.com>
2026-05-05 10:23:29 +0200

    temp

--- arch/x86/kvm/vmx/vmx_ops.h
+++ arch/x86/kvm/vmx/vmx_ops.h
@@ -13,8 +13,7 @@
 #define __ex(x) __kvm_handle_fault_on_reboot(x)
 
 asmlinkage void vmread_error(unsigned long field, bool fault);
-__attribute__((regparm(0))) void vmread_error_trampoline(unsigned long field,
-							 bool fault);
+void vmread_error_trampoline(unsigned long field, bool fault);
 void vmwrite_error(unsigned long field, unsigned long value);
 void vmclear_error(struct vmcs *vmcs, u64 phys_addr);
 void vmptrld_error(struct vmcs *vmcs, u64 phys_addr);
